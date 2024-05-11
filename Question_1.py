70eb37cc8bc43462b5145c27b3f704cbcfb2835e
samuelckraft <samuelckraft@gmail.com>
2024-05-11 13:51:55 -0400

    update

--- Question_1.py
+++ Question_1.py
@@ -1,1 +1,14 @@
 #Task 1
+import os
+
+def list_directory_contents(path):
+    try:
+        contents = os.listdir(path)
+        for file in contents:
+            print(f'Path {path} contains: {file}')
+    except FileNotFoundError:
+        print(f"Directory {path} not found")
+    except PermissionError:
+        print(f"Permission denied for {path}")
+
+list_directory_contents('')
---
+++ Question_2.py
@@ -0,0 +1,12 @@
+#Task 1
+import re
+
+def extract_emails(file):
+    with open(file, 'r') as file:
+        print('Emails: ')
+        for line in file:
+            emails = re.findall(r"[A-za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}", line)
+            for email in emails:
+                print(f'- {email}')
+
+extract_emails('contacts.txt')
